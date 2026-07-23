93f1e6634b8b0dcf36a2dedf882f010ebcbf7d0a
this-sunil <swarajya888@gmail.com>
2026-07-23 09:17:32 +0530

    initial setup jvm target

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,10 +28,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     defaultConfig {
